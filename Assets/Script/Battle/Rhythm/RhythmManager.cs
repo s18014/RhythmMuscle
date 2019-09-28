61a9cda986e485c43431473842d320dec9e19836
s18014 <s18014@std.it-college.ac>
2019-09-28 20:27:21 +0900

    ノーツの判定部分のプロトタイプを作成

--- Assets/Script/Battle/Rhythm/RhythmManager.cs
+++ Assets/Script/Battle/Rhythm/RhythmManager.cs
@@ -10,6 +10,9 @@ public class RhythmManager : MonoBehaviour
     [SerializeField] Image judgePoint;
     [SerializeField] Canvas canvas;
     float time = 0f;
+    float badTimeRange = 0.1f;
+    float judgeTimeRange = 0.1f;
+    int targetIndex = 0;
 
     private void Awake()
     {
@@ -20,6 +23,7 @@ public class RhythmManager : MonoBehaviour
             testNotes[i].transform.parent = canvas.transform;
             testNotes[i].GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
         }
+        testNotes[0].GetComponent<Note>().isTarget = true;
     }
 
     // Start is called before the first frame update
@@ -31,6 +35,12 @@ public class RhythmManager : MonoBehaviour
     void Update()
     {
         time += Time.deltaTime;
+        CheckTarget();
+        NoteUpdate();
+   }
+
+    void NoteUpdate()
+    {
         testNotes.ForEach(note =>
         {
             float judgePointX = judgePoint.GetComponent<RectTransform>().anchoredPosition.x;
@@ -38,10 +48,64 @@ public class RhythmManager : MonoBehaviour
             float speed = 3f;
             float posX = judgePointX + (note.GetComponent<Note>().judgeTime - time) * length * speed;
             note.GetComponent<RectTransform>().anchoredPosition = new Vector2(posX, 0f);
+            if (note.GetComponent<Note>().isTarget)
+            {
+                note.GetComponent<Image>().color = Color.red;
+            }
+            else
+            {
+                note.GetComponent<Image>().color = Color.green;
+            }
         });
     }
 
-    void Draw()
+    void judgeNote(Note note)
     {
+
+    }
+
+    void CheckTarget()
+    {
+        Note note = testNotes[targetIndex].GetComponent<Note>();
+        if (note.isTarget)
+        {
+            // タッチの判定内に来たかどうか
+            if (note.judgeTime + judgeTimeRange + badTimeRange >= time && note.judgeTime - judgeTimeRange - badTimeRange <= time)
+            {
+                // GOODの範囲かどうか
+                if (note.judgeTime + judgeTimeRange >= time && note.judgeTime - judgeTimeRange <= time)
+                {
+                    if (PlayerInput.touched)
+                    {
+                        Debug.Log("GOOD");
+                        ChangeNextTarget();
+                    }
+                }
+                else
+                {
+                    if (PlayerInput.touched)
+                {
+                        Debug.Log("Bad: Touch Out Of Range");
+                        ChangeNextTarget();
+                    }
+                }
+            }
+            // 判定時間 + 猶予時間が現在の時間を超えたら次のターゲットへ
+            if (note.judgeTime + judgeTimeRange < time)
+            {
+                Debug.Log("Bad: Time Over");
+                ChangeNextTarget();
+            }
+        }
+    }
+
+    void ChangeNextTarget()
+    {
+        testNotes[targetIndex].GetComponent<Note>().isTarget = false;
+        if (targetIndex+1 < testNotes.Count)
+        {
+            targetIndex++;
+            testNotes[targetIndex].GetComponent<Note>().isTarget = true;
+        }
     }
 }
